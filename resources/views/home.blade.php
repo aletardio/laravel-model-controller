200cacbcb2cb91cecb18243d6acda355abc47351
aletardio <ale.tardio99@gmail.com>
2024-02-14 15:32:52 +0100

    Created model and controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="centered-button">
+        <h1 class="text-center">Boolvideo</h1>
+        <a href="" class="btn btn-sm btn-primary d-flex justify-content-center">Visualizza Film</a>
+    </div>
+@endsection
