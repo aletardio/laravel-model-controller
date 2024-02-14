0755018bdfae248e548181a69e689f29fc78f7c1
aletardio <ale.tardio99@gmail.com>
2024-02-14 16:42:38 +0100

    Added cards to show movies

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
+
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('movies.index');
+        $movies = Movie::all();
+
+        return view('movies.index', compact('movies'));
     }
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,9 +4,24 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h2>
+                <h2 class="text-center pb-5">
                     I titoli dei film
                 </h2>
+            </div>
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="box-cards text-center d-flex justify-content-center">
+                        <div class="card cursor d-flex justify-content-center text-bg-dark mb-3" style="max-width: 18rem;">
+                            <h4 class="card-header">{{ $movie->title }}</h4>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie->original_title }}</h5>
+                                <p class="card-text">Lingua: {{ $movie->nationality }}</p>
+                                <p class="card-text">Data di uscita: {{ $movie->date }}</p>
+                                <p class="card-text">Voto: {{ $movie->vote }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
